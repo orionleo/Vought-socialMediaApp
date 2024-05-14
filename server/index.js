733c7ec4aabaff49ef9070b16937b9bb0314f8e6
Jai Aggarwal <jaiaggarwal60@gmail.com>
2024-05-14 20:35:20 +0530

    Fixed issues.

--- server/index.js
+++ server/index.js
@@ -6,22 +6,22 @@ import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/users.js";
 import dotenv from "dotenv";
 
-let app  = express();
+let app = express();
 dotenv.config();
 
-app.use(bodyParser.json({limit:"200mb",extended:true}));
-app.use(bodyParser.urlencoded({limit:"200mb",extended:true}));
+app.use(bodyParser.json({ limit: "200mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "200mb", extended: true }));
 app.use(cors());
-app.use('/posts',postRoutes);
-app.use('/user',userRoutes);
+app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("APP IS RUNNING");
 });
 
 let PORT = process.env.PORT;
-mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
-    .then(()=>app.listen(PORT,()=>console.log(`Server running on port: ${PORT}`)))
-    .catch((error)=>console.log(error.message));
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
 
 // mongoose.set('useFindAndModify',false);
